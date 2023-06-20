744ea63379c0fd38fa177729650807bd2e7013c2
soomni95 <rlatnals0507@gmail.com>
2023-06-20 16:41:31 +0900

    reordered

--- src/main/java/com/codestates/StackOverflow/question/repository/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codestates.StackOverflow.question.repository;
-
-import com.codestates.StackOverflow.question.entity.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Long>{
-}
---
+++ src/main/java/com/codestates/user/User.java
@@ -0,0 +1,37 @@
+package com.codestates.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "USERSS")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false, unique = true, name = "user_id")
+    private long user_id;
+    @Column(length = 50, nullable = false, updatable = true, unique = true, name = "email")
+    private String email;
+    @Column(length = 12, nullable = false, updatable = true, unique = true, name = "nickname")
+    private String nickname;
+    @Column(length = 12, nullable = false, updatable = true, unique = false, name = "password")
+    private String password;
+
+    public User(String email, String nickname, String password){
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+    }
+
+}
