98157edb56fd3d92fd6c192f8e15e0442615a106
jeongjwon <zlzlsksk111@gmail.com>
2023-06-20 19:03:22 +0900

    feat. 댓글 나열 구현

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -4,8 +4,33 @@ import { styled } from "styled-components";
 import { YourAnswerInputDiv } from "./YourAnswer";
 export const CommnetContainer = styled.div`
   /* color: #6a737c; */
+  > div:first-child {
+    border-top: 0.7px solid #ccc;
+  }
+`;
+export const CommentList = styled.div`
+  /* border-top: 0.7px solid #ccc; */
+  border-bottom: 0.7px solid #ccc;
+  padding: 12px;
+  padding-left: 30px;
+  font-size: 12px;
+  text-align: left;
+
+  > span:first-child {
+    font-weight: 600;
+  }
+  > span:nth-child(2) {
+    color: #0074cc;
+  }
+  > span:last-child {
+    color: #7d8387;
+  }
+  > span:not(:first-child) {
+    cursor: pointer;
+  }
 `;
 export const AddComment = styled.div`
+  margin-top: 20px;
   button {
     color: #6a737c;
     cursor: pointer;
@@ -27,6 +52,19 @@ export const AddCommentInput = styled(YourAnswerInputDiv)`
 `;
 
 const Comment = () => {
+  // 임의 댓글 데이터
+  const comments = [
+    {
+      userName: "이무진",
+      comment: "가을 타나봐",
+      // createdAt: , 날짜
+    },
+    {
+      userName: "아이유",
+      comment: "정류장",
+      // createdAt: , 날짜
+    },
+  ];
   const [newComment, setNewComment] = useState("");
   const [addComment, setAddComment] = useState(false);
   const hanldeAddComment = () => {
@@ -45,7 +83,15 @@ const Comment = () => {
     <>
       <CommnetContainer>
         {/* 답변이 있는 경우 comment 들만 */}
-
+        {comments.map((comment, idx) => {
+          return (
+            <CommentList key={idx}>
+              <span>{comment.comment}</span>
+              <span> -{comment.userName}</span>
+              <span> 날짜</span>
+            </CommentList>
+          );
+        })}
         {/* 답변 추가 구역*/}
         <AddComment>
           <button onClick={hanldeAddComment}>Add a comment</button>
