5510db14f78e978e849874e3edb78581437e26c9
gustn5309 <124798079+gustn5309@users.noreply.github.com>
2023-06-22 00:24:54 +0900

    feat/be/profile 초기화 및 중간통합용

---
+++ server/pre/src/main/java/com/codestates/answer/entity/Answer.java
@@ -0,0 +1,41 @@
+package com.codestates.answer.entity;
+
+
+import com.codestates.question.entity.Question;
+import com.codestates.user.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long answerId;
+
+    @Column(nullable = false)
+    private String body;
+
+    @Column(nullable = false)
+    private boolean isSelected;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column
+    private LocalDateTime modifiedAt = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "QUESTION_ID")
+    private Question question;
+
+    @ManyToOne
+    @JoinColumn(name = "userid")
+    private User user;
+}
--- server/src/main/java/com/codestates/security/jwt/dto/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package com.codestates.security.jwt.dto;
-
-import lombok.Getter;
-
-@Getter
-public class LoginDto {
-    private String email;
-    private String password;
-}
