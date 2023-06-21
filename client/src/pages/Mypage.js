6b4238883328ba79fea65564352be51eacd9f09c
HyoJeong-Park <parkhj3003@gmail.com>
2023-06-21 19:11:43 +0900

    fix. mypage 라우터 수정

--- client/src/pages/Mypage.js
+++ client/src/pages/Mypage.js
@@ -4,96 +4,87 @@ import { HiPencil } from "react-icons/hi";
 import { FaRegCalendarAlt } from "react-icons/fa";
 import { MdCake } from "react-icons/md";
 import { AiOutlineClockCircle } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
-const Mypage = () => {
-  const MypageWrap = styled.section`
-    width: 1000px;
-    text-align: left;
-    float: right;
-    padding: 30px;
-    // 컴포넌트, 페이지 다 만들고 레이아웃 배치하기
-  `;
+const MypageWrap = styled.section`
+  width: 1000px;
+  text-align: left;
+  float: right;
+  padding: 30px;
+  // 컴포넌트, 페이지 다 만들고 레이아웃 배치하기
+`;
 
-  const ProfileBtn = styled.button`
-    position: absolute;
-    top: 0;
-    right: 0;
-    padding: 7px 10px;
-    border: 1px solid #545454;
-    margin: 0 5px;
-    border-radius: 5px;
-    color: #545454;
-    font-size: 15px;
-    background-color: #fff;
-
-    &:hover {
-      cursor: pointer;
-    }
-
-    span {
-      padding: 0 10px 0 5px;
-    }
-  `;
-
-  const MypageProfile = styled.div`
-    display: flex;
-    position: relative;
-    color: #545454;
-
-    div.profileContents {
-      padding: 0 10px;
-
-      h1 {
-        margin-left: 13px;
-      }
-
-      p {
-        display: inline;
-        padding: 0 10px;
-        font-size: 14px;
-      }
-
-      svg {
-        padding: 0 5px;
-      }
-    }
-  `;
-
-  const MypageCategoryWrap = styled.ul`
-    list-style: none;
-    padding: 0;
-    margin: 30px 0;
-
-    .active {
-      background-color: #f4852b;
-      color: #fff;
-    }
-  `;
-
-  const MypageCategoty = styled.li`
-    display: inline-block;
-    border-radius: 20px;
-    padding: 5px 20px;
+const ProfileBtn = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 7px 10px;
+  border: 1px solid #545454;
+  margin: 0 5px;
+  border-radius: 5px;
+  color: #545454;
+  font-size: 15px;
+  background-color: #fff;
+  &:hover {
     cursor: pointer;
-    font-size: 15px;
-  `;
+  }
+  span {
+    padding: 0 10px 0 5px;
+  }
+`;
 
-  const PostWrap = styled.div`
-    position: relative;
-    margin: 5px 0 10px 0;
-
-    div {
-      width: 70%;
-      height: 100px;
-      border: 1px solid #d9d9d9;
-      border-radius: 5px;
+const MypageProfile = styled.div`
+  display: flex;
+  position: relative;
+  color: #545454;
+  div.profileContents {
+    padding: 0 10px;
+    h1 {
+      margin-left: 13px;
     }
-
-    &:last-child h1 {
-      margin-top: 30px;
+    p {
+      display: inline;
+      padding: 0 10px;
+      font-size: 14px;
     }
-  `;
+    svg {
+      padding: 0 5px;
+    }
+  }
+`;
 
+const MypageCategoryWrap = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 30px 0;
+  .active {
+    background-color: #f4852b;
+    color: #fff;
+  }
+`;
+
+const MypageCategoty = styled.li`
+  display: inline-block;
+  border-radius: 20px;
+  padding: 5px 20px;
+  cursor: pointer;
+  font-size: 15px;
+`;
+
+const PostWrap = styled.div`
+  position: relative;
+  margin: 5px 0 10px 0;
+  div {
+    width: 70%;
+    height: 100px;
+    border: 1px solid #d9d9d9;
+    border-radius: 5px;
+  }
+  &:last-child h1 {
+    margin-top: 30px;
+  }
+`;
+const Mypage = () => {
   return (
     <MypageWrap>
       <MypageProfile>
@@ -113,16 +104,20 @@ const Mypage = () => {
             Visited 3 days, 3 consecutive
           </p>
         </div>
-        <ProfileBtn>
-          <HiPencil size={15} />
-          <span>Edit profile</span>
-        </ProfileBtn>
+        <Link to="/mypage/edit">
+          <ProfileBtn>
+            <HiPencil size={15} />
+            <span>Edit profile</span>
+          </ProfileBtn>
+        </Link>
       </MypageProfile>
       <MypageCategoryWrap>
         <MypageCategoty>Profile</MypageCategoty>
         <MypageCategoty className="active">Activity</MypageCategoty>
         <MypageCategoty>Saves</MypageCategoty>
-        <MypageCategoty>Settings</MypageCategoty>
+        <Link to="/mypage/edit">
+          <MypageCategoty>Settings</MypageCategoty>
+        </Link>
       </MypageCategoryWrap>
       <PostWrap>
         <h1>Likes</h1>
