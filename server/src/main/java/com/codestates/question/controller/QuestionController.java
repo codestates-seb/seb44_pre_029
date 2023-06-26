77dc8eba61ce903bda1b2d9bf8e1b7b9f8128f6d
gustn5309 <124798079+gustn5309@users.noreply.github.com>
2023-06-26 18:13:54 +0900

    mypage 회원이 작성한 글 불러오기 추가

--- server/src/main/java/com/codestates/question/controller/QuestionController.java
+++ server/src/main/java/com/codestates/question/controller/QuestionController.java
@@ -7,8 +7,12 @@ import com.codestates.question.mapper.QuestionMapper;
 import com.codestates.question.service.QuestionService;
 import com.codestates.response.PageInfo;
 import com.codestates.response.QuestionPageResponseDto;
+import com.codestates.user.User;
+import com.codestates.user.UserRepository;
+import com.codestates.user.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -28,12 +32,18 @@ import java.util.stream.Collectors;
 public class QuestionController {
     private final static String Question_DEFAULT_URL = "/questions";
     private final QuestionService questionService;
+    private final UserService userService;
+    private final UserRepository userRepository;
     private final QuestionMapper mapper;
 
     public QuestionController(QuestionService questionService,
+                              UserService userService,
+                              UserRepository userRepository,
                               QuestionMapper mapper) {
         this.questionService = questionService;
         this.mapper = mapper;
+        this.userRepository = userRepository;
+        this.userService = userService;
     }
 
     @PostMapping("/create")
@@ -75,6 +85,29 @@ public class QuestionController {
         Page<Question> QuestionPage = questionService.findQuestions(page - 1, size);
         PageInfo pageInfo = new PageInfo(page, size,(int)QuestionPage.getTotalElements(), QuestionPage.getTotalPages());
         List<Question>questions = QuestionPage.getContent();
+
+
+        List<QuestionDto.GETResponse> response =
+                questions.stream()
+                        .map(question-> mapper.questionToQuestionGETResponseDto(question))
+                        .collect(Collectors.toList());
+
+
+        return new ResponseEntity<>(new QuestionPageResponseDto(response, pageInfo), HttpStatus.OK);
+    }
+//유저가 작성한 게시글 불러오기
+    @GetMapping("/{user-id}/userquestion")
+    public ResponseEntity getUserQuestions(@PathVariable("user-id")@Positive Long userId,
+                                           @Positive @RequestParam int page,
+                                            @Positive @RequestParam int size,
+                                           Authentication authentication){
+        Map<String, Object> principal = (Map) authentication.getPrincipal();
+        User user = userRepository.findByUserId(((Number) principal.get("userId")).longValue()).orElse(null);
+
+        Page<Question> questionPage = questionService.findUserQuestions(user,page,size);
+        PageInfo pageInfo = new PageInfo(page, size,(int)questionPage.getTotalElements(), questionPage.getTotalPages());
+
+        List<Question>questions = questionService.findUsersQuestions(user);
 
 
         List<QuestionDto.GETResponse> response =
